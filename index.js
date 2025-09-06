2a28788fca7ebc795f8c09845b9a2b01fa4e1668
saidulislam-official <saidulislam.own@gmail.com>
2025-09-06 18:57:04 +0600

    added js fetch

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+fetchData()
+async function fetchData() {    
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
 
+        if (!response.ok){
+            throw new Error("could not fetch resources");
+        }
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
