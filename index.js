49043613f41fe0161aaccd916b07124483c96991
saidulislam-official <saidulislam.own@gmail.com>
2025-09-06 19:22:47 +0600

    final production done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-fetchData()
+// fetchData()
 async function fetchData() {    
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if (!response.ok){
             throw new Error("could not fetch resources");
         }
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = 'block';
+
     }
     catch(error){
         console.error(error);
